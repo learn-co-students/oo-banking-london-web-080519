14174cd69e1a707fdd9ebccc4a2d33af04e8fc9e
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-08 15:44:01 +0100

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
+#require "pry"
+
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status, :is_valid
+
+   # @@all = []
+
+    def initialize(owner)
+        @name = owner
+        @balance = 1000
+        @status = "open"
+       # @@all << self
+    end
+
+    def deposit(amount) #just method for the instance, not the class
+        @balance += amount #get the instance of @balance
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}." #call it on this instance's balance
+    end
+
+    def valid?
+        # == true if @status == "open"
+        # && @balance > 0
+        self.status == "open" && self.balance > 0 
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
+    def self.all
+        @@all
+    end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,70 @@
+require "pry"
+
+require_relative "bank_account.rb"
+
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  @@all = []
+
+  def all
+    @@all
+  end
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+
+  def valid?
+    #get to each bank account
+    #BankAccount.all.select {|account| account.status == "open"}
+    # BankAccount.all.select {|account| account.name == self}
+
+    @sender.valid? && @receiver.valid?
+
+    #BankAccount.all.select {|account| account.valid? }
+    #if each of our instances.valid? == true
+    #then the transfer.valid? == true
+
+  end
+
+  def execute_transaction
+    #amount from sender to receiver
+    ## subtract amount from sender's balance
+    ## add amount to receiver's balance
+
+    if valid? && @status == "pending" && @sender.balance > @amount 
+        #run .valid? on transfer on our @sender (in transfer)
+          self.sender.balance -= @amount  #in bank_account. 
+          self.receiver.balance += @amount
+          @status = "complete"
+          @@all << self
+      else  
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+  end
+
+  def reverse_transfer
+    #reverse transfer between two accounts
+    #can only reverse executed transfers
+
+    if status == "complete"
+      self.sender.balance += @amount
+      self.receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
+
+  
 end
+# user2 = BankAccount.new("erin")
+# user1 = BankAccount.new("will")
+
+# binding.pry
+# 0
